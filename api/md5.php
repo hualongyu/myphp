098fed93c9d7c3116be0fcbbfa73135e3df6a924
hualongyu <86827942+hualongyu@users.noreply.github.com>
2023-08-23 09:20:39 +0800

    Create md5.php

---
+++ api/md5.php
@@ -0,0 +1,6 @@
+<?php
+$md5 = "2ba3daaf52719d0a7a6f58a787b727b0";
+$uid = "hhp-ImZRY";
+$token = strtolower(md5('1878399009'));
+$url = "http://www.ttmd5.com/do.php?c=Api&m=crack&uid=$uid&token=$token&cipher=$md5";$data = file_get_contents($ur1);echo $data;
+?>
