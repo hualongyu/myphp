1970e0f891a6f72b0245bf1eff92ee73ba7f1bbe
hualongyu <86827942+hualongyu@users.noreply.github.com>
2023-11-06 14:27:05 +0800

    Update gudou.php

--- api/gudou.php
+++ api/gudou.php
@@ -3,15 +3,15 @@ header('Content-Type:application/json;charset=utf-8');
 $id = $_GET['id'];
 $playseek = $_GET['playseek'];
 
-$user = '13392382276';//改成你的名字
+$user = '18998910474';//改成你的名字
 $pw = strtoupper(md5('yu22425335')); //123456改成你的密码
-$pserialnumber ='5d84cb6c4f9a94d4'; //改成你的设备ID
+$pserialnumber ='61bc79e2f63c4cd8'; //改成你的设备ID
 $t = time();
 $nonce = rand(100000,999999);
 $str = 'sumasalt-app-portalpVW4U*FlS'.$t.$nonce.$user;
 $hmac = substr(sha1($str),0,10);
 
-$ptoken = 'zBbw9PLkmZ8ZON%2Fb9QyBZA%3D%3D'; //失效时抓包登录信息，修改值
+$ptoken = 'iG7pVn7s%2FzuD1QnflH%2FIjQ%3D%3D'; //失效时抓包登录信息，修改值
 $newtoken = $ptoken;
 
 $url="http://portal.gcable.cn:8080/PortalServer-App/new/aaa_aut_aut002?ptype=1&plocation=001&puser=$user&ptoken=$ptoken&pversion=030107&pserverAddress=portal.gcable.cn&pserialNumber=$pserialnumber&pkv=1&ptn=Y29tLnN1bWF2aXNpb24uc2FucGluZy5ndWRvdQ&DRMtoken=&epgID=&authType=0&secondAuthid=&t=$ptoken&pid=&cid=364&u=$user&p=1&l=001&d=$pserialnumber&n=$id&v=2&ot=0&pappName=GoodTV&hmac=$hmac&timestamp=$t&nonce=$nonce";
